d8f9ab36054e1feff162dcb7e36bfeb800d4a3c5
GemaKhusnulMarifah <113269371+GemaKhusnulMarifah@users.noreply.github.com>
2023-09-21 09:36:16 +0700

    updatejava

---
+++ assets/changing.js
@@ -0,0 +1,13 @@
+var tabLinks = document.getElementsByClassName("tablink");
+var tabContents = document.getElementsByClassName("tabcontent");
+
+function openTab(tabname) {
+for (var tabLink of tabLinks) {
+    tabLink.classList.remove("activelink");
+}
+for (var tabContent of tabContents) {
+    tabContent.classList.remove("activetab");
+}
+event.currentTarget.classList.add("activelink");
+document.getElementById(tabname).classList.add("activetab");
+}
